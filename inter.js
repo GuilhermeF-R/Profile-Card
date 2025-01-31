bc883bc733a9fe4562370ce575d4e8f6e907056d
GuilhermeF-R <Frodriguesguilherme18@gmail.com>
2025-01-31 20:49:03 -0300

    ajuste de erros de pastas

---
+++ inter.js
@@ -0,0 +1,23 @@
+  // Function to toggle the class and apply or remove the rotation
+  function toggleRotation() {
+    var cardElement = document.querySelector('.CARD');
+    cardElement.classList.toggle('rotate');
+    
+    // Add a test console.log
+    console.log('The toggleRotation() function was called.');
+}
+/*
+const url ="https://github.com/GuilhermeF-R"
+const git = document.querySelector('button.github')
+
+function openInNewTab(){
+    const win = window.open(url, '_blank')
+    win.focus()
+}
+
+git.addEventListener('click', () => {
+    openInNewTab(url)
+})*/
+/*site feito por GUILHERME FERREIRA ou DevFerreira*/
+/*"https://github.com/GuilhermeF-R"*/
+
